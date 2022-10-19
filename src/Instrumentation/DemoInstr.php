cbdecbaa9f6608da64b9fd9c3fe67bfdb8d929db
Brett McBride <brett@deakin.edu.au>
2022-10-19 22:20:20 +1100

    work around DebugScope when using span->activate, a DebugScope is created but gets GC'd, which emits bogus warnings that scopes haven't been detached.

--- src/Instrumentation/DemoInstr.php
+++ src/Instrumentation/DemoInstr.php
@@ -12,12 +12,13 @@ class DemoInstr
 {
     public static function init(): void
     {
-        $instrumentation = new CachedInstrumentation('');
+        $instrumentation = new CachedInstrumentation('demo');
         hook(
             Demo::class,
             'run',
             static function (Demo $demo, array $params, string $class, string $function, ?string $filename, ?int $lineno) use ($instrumentation) {
-                $instrumentation->tracer()->spanBuilder(sprintf('%s:%s', $class, $function))->startSpan()->activate();
+                $span = $instrumentation->tracer()->spanBuilder(sprintf('%s:%s', $class, $function))->startSpan();
+                Context::storage()->attach($span->storeInContext(Context::getCurrent()));
             },
             static function (Demo $demo, array $params, ?string $return, ?Throwable $exception) {
                 $scope = Context::storage()->scope();
